e0cc494829080b6d7b1ec68cffc723c5f5950259
SirRobert-1 <rviverosgonzalez@gmail.com>
2023-05-16 15:37:42 -0600

    feat: Productos dinamicos

--- src/pages/productos.jsx
+++ src/pages/productos.jsx
@@ -1,5 +1,55 @@
 import React from "react";
 
-export default function productos() {
-  return <div>Productos</div>;
+const productosData = [
+  {
+    id: 1,
+    nombre: "Producto 1",
+    precio: 100,
+    descripcion: "Descripcion del producto 1",
+    ubicacion: "Ubicacion del producto 1",
+    reviews: [
+      {
+        id: 1,
+        comentario: "Comentario 1",
+      },
+      {
+        id: 2,
+        comentario: "Comentario 2",
+      },
+    ],
+  },
+  {
+    id: 2,
+    nombre: "Producto 2",
+    precio: 200,
+    descripcion: "Descripcion del producto 2",
+    ubicacion: "Ubicacion del producto 2",
+    reviews: [
+      {
+        id: 1,
+        comentario: "Comentario 1",
+      },
+      {
+        id: 2,
+        comentario: "Comentario 2",
+      },
+    ],
+  },
+];
+
+export default function Productos() {
+  return (
+    <div>
+      {productosData.map((producto) => {
+        return (
+          <div key={producto.id}>
+            <h1>{producto.nombre}</h1>
+            <h2>{producto.precio}</h2>
+            <h3>{producto.descripcion}</h3>
+            <h4>{producto.ubicacion}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
